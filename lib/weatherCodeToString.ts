011484816e459ec5343fdcf9ff619fd392999308
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-07-26 22:02:19 -0500

    Gather's string and label information for various weather codes to display an icon and text

---
+++ lib/weatherCodeToString.ts
@@ -0,0 +1,119 @@
+export const weatherCodeToString: {
+  [key: number]: {
+    icon: string;
+    label: string;
+  };
+} = {
+  0: {
+    icon: 'c01d',
+    label: 'Clear sky',
+  },
+  1: {
+    icon: 'c02d',
+    label: 'Mainly clear',
+  },
+  2: {
+    icon: 'c02d',
+    label: 'Partly cloudy',
+  },
+  3: {
+    icon: 'c04d',
+    label: 'Overcast',
+  },
+  45: {
+    icon: 'a05d',
+    label: 'Fog',
+  },
+  48: {
+    icon: 'a05d',
+    label: 'Depositing rime fog',
+  },
+  51: {
+    icon: 'd02d',
+    label: 'Drizzle',
+  },
+  53: {
+    icon: 'd02d',
+    label: 'Drizzle',
+  },
+  55: {
+    icon: 'd02d',
+    label: 'Drizzle',
+  },
+  56: {
+    icon: 'd02d',
+    label: 'Freezing Drizzle',
+  },
+  57: {
+    icon: 'd02d',
+    label: 'Freezing Drizzle',
+  },
+  61: {
+    icon: 'r01d',
+    label: 'Light Rain',
+  },
+  63: {
+    icon: 'r02d',
+    label: 'Moderate Rain',
+  },
+  65: {
+    icon: 'r03d',
+    label: 'Heavy Rain',
+  },
+  66: {
+    icon: 'f01d',
+    label: 'Freezing Rain',
+  },
+  67: {
+    icon: 'f01d',
+    label: 'Freezing Rain',
+  },
+  71: {
+    icon: 's01d',
+    label: 'Light Snow',
+  },
+  73: {
+    icon: 's02d',
+    label: 'Moderate Snow',
+  },
+  75: {
+    icon: 's03d',
+    label: 'Heavy Snow',
+  },
+  77: {
+    icon: 's01d',
+    label: 'Snow Grains',
+  },
+  80: {
+    icon: 'r04d',
+    label: 'Light Rain Showers',
+  },
+  81: {
+    icon: 'r05d',
+    label: 'Moderate Rain Showers',
+  },
+  82: {
+    icon: 'r06d',
+    label: 'Violent Rain Showers',
+  },
+  85: {
+    icon: 's01d',
+    label: 'Light Snow Showers',
+  },
+  86: {
+    icon: 's02d',
+    label: 'Heavy Snow Showers',
+  },
+  95: {
+    icon: 't02d',
+    label: 'Thunderstorm',
+  },
+  96: {
+    icon: 't05d',
+    label: 'Thunderstorm with Light Hail',
+  },
+  99: {
+    icon: 't05d',
+    label: 'Thunderstorm with Heavy Hail',
+  },
+};
