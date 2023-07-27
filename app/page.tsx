806c4f3e8c8649ef27b0b2ade2233341f0216eeb
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-07-26 21:56:41 -0500

    linting fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-
-import CityPicker from "@/components/CityPicker"
-import { Card, Divider, Subtitle, Text } from "@tremor/react"
+import { Card, Divider, Subtitle, Text } from '@tremor/react';
+import CityPicker from '@/components/CityPicker';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#395299] to-[#203b85] p-10 flex flex-col justify-center items-center">
       <Card>
         <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
-        <Subtitle className="text-xl text-center">Powered by Next.js 13.4, Tailwind CSS, Tremor 2.0 + More!</Subtitle>
+        <Subtitle className="text-xl text-center">
+          Powered by Next.js 13.4, Tailwind CSS, Tremor 2.0 + More!
+        </Subtitle>
 
-        <Divider className="my10"/>
+        <Divider className="my10" />
 
         <Card className="bg-gradient-to-br from-[#395299] to-[#203b85]">
-          <CityPicker/>
+          <CityPicker />
         </Card>
       </Card>
     </div>
-  )
+  );
 }
