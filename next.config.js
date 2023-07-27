6a1acd562326e3ec063683b840ac2872c7a39fbf
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-07-26 19:58:29 -0500

    next.js config for image loading

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['cdn.weatherbit.io'],
+    loaders: {
+      'png': 'image-webpack-loader',
+    },
+  },
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["@tremor/react"]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
